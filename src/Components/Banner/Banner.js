f2f6c4124c58d66b981820dd9734bad47e347f79
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-06-09 02:34:00 +0600

    banner set

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import './Banner.css'
 import shuvo from '../../images/banner/shuvo.jpg';
+import rsm from '../../images/pdf/My Resume.pdf';
 
 const Banner = () => {
     return (
         <div>
-            <div className='d-flex align-items-center p-5 mt-5 bg-success p-2 text-dark bg-opacity-10 justify-content-evenly'>
-                <div>
-                    <h2>Dr.Freddie Highmore</h2>
+            <div className='d-flex align-items-center p-5 mt-5 bg-success p-2 text-dark bg-opacity-10 justify-content-evenly work'>
+                <div className='App'>
+                    <h2 className='font-weight-bold'>Shabbir Ahmed Shuvo</h2>
+                    <p className='mt-3 mx-auto'>I am a web developer . building websites and web applications. I specialize in JavaScript and have professional experience working with reactJs, nodejs and mongodb. I also done some project by reactJs, . Take a look at my work or get in touch</p>
                     <br />
-                    <p>Let the young know they will never find a more interesting, more instructive book than the patient himself. <br /> True health care reform cannot happen in Washington. It has to happen in our kitchens, <br /> in our homes, in our communities. All health care is personal</p>
+                    <a href={rsm} className='btn btn-primary btn-lg' download={rsm} >Download Resume</a>
                 </div>
                 <div>
                     <img src={shuvo} alt="" />

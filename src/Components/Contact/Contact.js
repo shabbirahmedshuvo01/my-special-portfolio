72edb2afbfdf0b440cfecf58e05569055166fecf
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-06-09 20:45:47 +0600

    submit set up

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,16 +1,31 @@
 import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
 
     const emailRef = useRef('');
     const descriptionRef = useRef('');
+    const nameRef = useRef('');
 
-    const handleSubmit = async event => {
+    const handleSubmit = event => {
         event.preventDefault();
+        const name = event.target.name.value
+        const subject = event.target.subject.value
         const email = emailRef.current.value;
         const description = descriptionRef.current.value;
 
-        console.log(email, description);
+        console.log(name, subject, email, description)
+
+        emailjs.sendForm('service_h5syjfe', 'template_od2ekb8', event.target, '4rjlS9y1cer7zxOdj')
+            .then((result) => {
+                console.log(result.text);
+                if (result.text) {
+                    alert('Mail Sent')
+                }
+            }, (error) => {
+                console.log(error.text);
+            });
+        event.target.reset();
     }
 
     return (
@@ -20,11 +35,19 @@ const Contact = () => {
 
             <form onSubmit={handleSubmit}>
                 <div class="form-floating mt-3">
-                    <input ref={emailRef} type="email" class="form-control" id="floatingInput" placeholder="Email" />
+                    <input ref={nameRef} type="name" name="name" class="form-control" id="floatingInput" placeholder="Name" />
+                    <label for="floatingInput">Your Name</label>
+                </div>
+                <div class="form-floating mt-3">
+                    <input type="subject" name="subject" class="form-control" id="floatingInput" placeholder="subject" />
+                    <label for="floatingInput">Subject</label>
+                </div>
+                <div class="form-floating mt-3">
+                    <input ref={emailRef} type="email" name="email" class="form-control" id="floatingInput" placeholder="Email" />
                     <label for="floatingInput">Email address</label>
                 </div>
                 <div class="form-floating mt-3">
-                    <textarea ref={descriptionRef} type="description" class="form-control" placeholder="Description" />
+                    <textarea ref={descriptionRef} type="description" name='description' class="form-control" placeholder="Description" />
                     <label for="floatingPassword">Description</label>
                 </div>
                 <input className='btn btn-primary mt-3' type="submit" value="Send Mail" />

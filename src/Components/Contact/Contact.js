5bf6bc2ff44019dc32834f1ea666b65210cfb721
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-06-09 18:33:50 +0600

    submit set up

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Contact = () => {
+
+    const emailRef = useRef('');
+    const descriptionRef = useRef('');
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const description = descriptionRef.current.value;
+
+        console.log(email, description);
+    }
+
     return (
         <div className='w-50 mx-auto'>
 
             <h2 className='mt-5'>Contact Me</h2>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div class="form-floating mt-3">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="Email" />
+                    <input ref={emailRef} type="email" class="form-control" id="floatingInput" placeholder="Email" />
                     <label for="floatingInput">Email address</label>
                 </div>
                 <div class="form-floating mt-3">
-                    <textarea type="description" class="form-control" placeholder="Description" />
+                    <textarea ref={descriptionRef} type="description" class="form-control" placeholder="Description" />
                     <label for="floatingPassword">Description</label>
                 </div>
                 <input className='btn btn-primary mt-3' type="submit" value="Send Mail" />
